1c2e1a6a0db3626c800db497dbef86377542d7ce
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2022-08-26 13:34:26 +0530

    new ext added

--- dashfilebrowser/filebrowser.py
+++ dashfilebrowser/filebrowser.py
@@ -1,10 +1,10 @@
-from genericpath import isdir, isfile
 import os
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+from genericpath import isdir, isfile
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -17,26 +17,35 @@ PWD = os.path.abspath(root)
 IFFILE = False
 FILEPATH = "none"
 
+FILEEXT = [".tex", ".txt", ".py", ".jl", ".md"]
+
+
+def iffileis(name): return any(
+    map(lambda x: name[-len(x):] == x if (x in name) else False, FILEEXT))
+
+
 def fixpath(path):
     global PWD
     return os.path.abspath(path).replace(PWD, "ROOT")
 
+
 def listfiles(folder):
     global PWD
     folder = folder.replace("ROOT", ".")
-    l = [folder +"/"+ i for i in os.listdir(folder)]
+    l = [folder + "/" + i for i in os.listdir(folder)]
     l = [i for i in l if "/." not in i]
-    l = [i for i in l if ((".tex" in i) or os.path.isdir(i))]
+    l = [i for i in l if (iffileis(i) or os.path.isdir(i))]
     l_dir = [i for i in l if os.path.isdir(i)]
     l_dir.sort()
     l_fil = [i for i in l if not(os.path.isdir(i))]
     l_fil.sort()
     l = l_dir+l_fil
     l = [fixpath(i) for i in l]
-    if os.path.abspath(folder)==PWD:
+    if os.path.abspath(folder) == PWD:
         l = [fixpath(folder+"/.")] + l
     else:
-        l = list(set([fixpath(i) for i in [folder+"/.", ".", folder+"/.."]])) + l
+        l = list(set([fixpath(i)
+                 for i in [folder+"/.", ".", folder+"/.."]])) + l
     return l
 
 
@@ -50,21 +59,21 @@ controls = [
     )
 ]
 
-app.layout = html.Div( html.Div(
+app.layout = html.Div(html.Div(
     [html.H1("File Browser"),
-    html.Div(controls),
-    html.Div(id='status2',
-             children=''),
-    dcc.Textarea(
+     html.Div(controls),
+     html.Div(id='status2',
+              children=''),
+     dcc.Textarea(
         id='textarea',
         value='Empty',
-        style={'width': '100%', 'height': 300},
+        style={'width': '100%', 'height': 600},
     ),
-    html.Button('Save', id='submit', n_clicks=0),
-    html.Div(id='status',
-             children=''),
+        html.Button('Save', id='submit', n_clicks=0),
+        html.Div(id='status',
+                 children=''),
     ],
-    style={"max-width": "900px", "margin":"auto"}
+    style={"max-width": "1200px", "margin": "auto"}
 ))
 
 
@@ -75,6 +84,7 @@ def list_all_files(folder_name):
         folder_name = os.path.dirname(folder_name)
     files = listfiles(folder_name)
     return [{"label": x, "value": x} for x in files]
+
 
 @app.callback(Output("textarea", "value"), Input("dropdown", "value"))
 def list_all_files(file_name):
@@ -90,6 +100,7 @@ def list_all_files(file_name):
         FILEPATH = "none"
     return text
 
+
 @app.callback(Output("status2", "children"), Input("dropdown", "value"))
 def list_all_files(file_name):
     global IFFILE, FILEPATH
@@ -102,17 +113,22 @@ def list_all_files(file_name):
         FILEPATH = "none"
     return "FILEPATH: "+FILEPATH
 
+
 @app.callback(Output("status", "children"), [Input("submit", "n_clicks")], [State('textarea', 'value')])
 def save(nclick, text):
     global IFFILE, FILEPATH
-    if IFFILE and text!="Empty":
+    if IFFILE and text != "Empty":
         with open(FILEPATH, "w") as f:
             for line in text.split("\n"):
                 f.write(line+"\n")
     return "SAVED FILEPATH: "+FILEPATH
 
+
 def run():
     app.run_server(debug=True)
 
+
 if __name__ == "__main__":
     app.run_server(debug=True)
+
+
